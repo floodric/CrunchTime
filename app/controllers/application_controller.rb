6a1d2e2e84108928e76643587f027bec16f0e53b
Ryan Flood <rflood@cmu.edu>
2013-09-15 04:50:29 -0400

    you can now create users, cant figure out sessions yet
    insert witty comment about jam sessions here

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,6 @@ class ApplicationController < ActionController::Base
     redirect_to root_url#, :alert => exception.message
   end
 
-  private
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
