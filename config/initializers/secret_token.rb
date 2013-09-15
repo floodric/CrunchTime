424f71385ec94e98bd0d8586cf8bbf4bc2b15a46
Ryan Flood <rflood@cmu.edu>
2013-09-15 03:56:48 -0400

    initial commit, ignore the timestamp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrunchTime::Application.config.secret_token = 'b6eedb6fde48976a145505c6aed500523446bd8aaa0042f7f8c3c29f4d162a730cd5718088770f0008516ca4c2aa9354f937fcd0ab35f57bb9cc52466be349bb'
